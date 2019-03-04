f30b32e9b24ac4aed2443b001671697843d2c0b1
mikkelkai <mikkelkai@gmail.com>
2019-03-04 16:15:30 +0100

    added TrackRelaxed

---
+++ database/t/test_track_relaxed.py
@@ -0,0 +1,29 @@
+from database.track_relaxed import TrackRelaxed
+from database.storinator import Storinator
+import datetime
+
+
+def test_implements_Storinator():
+    tbpm = TrackRelaxed()
+    assert isinstance(tbpm, Storinator)
+
+
+def test_database_name():
+    tbpm = TrackRelaxed()
+    assert tbpm._dbname == 'track_relaxed'
+
+
+def test_add_and_get():
+    tbpm = TrackRelaxed()
+    tbpm.add(1, 'relaxed', 3.)
+    track = tbpm.get(1)
+    assert track['song_id'] == 1
+    assert track['relaxed'] == 'relaxed'
+    assert track['confidence'] == 3.
+
+
+def test_get_all():
+    tbpm = TrackRelaxed()
+    tbpm.add(1, 'relaxed', 3.)
+    tracks = tbpm.get_all()
+    assert len(tracks) > 0
