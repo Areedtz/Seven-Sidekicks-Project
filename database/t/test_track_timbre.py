8d788ae7d1a040c7b43093b80da933bfb8e54759
mikkelkai <mikkelkai@gmail.com>
2019-03-04 16:09:18 +0100

    TrackTimbre and additional tests

--- database/t/test_track_timbre.py
+++ database/t/test_track_timbre.py
@@ -1,29 +1,29 @@
-from database.track_bpm import TrackBPM
+from database.track_timbre import TrackTimbre
 from database.storinator import Storinator
 import datetime
 
 
 def test_implements_Storinator():
-    tbpm = TrackBPM()
+    tbpm = TrackTimbre()
     assert isinstance(tbpm, Storinator)
 
 
 def test_database_name():
-    tbpm = TrackBPM()
-    assert tbpm._dbname == 'track_bpm'
+    tbpm = TrackTimbre()
+    assert tbpm._dbname == 'track_timbre'
 
 
 def test_add_and_get():
-    tbpm = TrackBPM()
-    tbpm.add(1, 100., 3.)
+    tbpm = TrackTimbre()
+    tbpm.add(1, 'bright', 3.)
     track = tbpm.get(1)
     assert track['song_id'] == 1
-    assert track['bpm'] == 100.
+    assert track['timbre'] == 'bright'
     assert track['confidence'] == 3.
 
 
 def test_get_all():
-    tbpm = TrackBPM()
-    tbpm.add(1, 100., 3.)
+    tbpm = TrackTimbre()
+    tbpm.add(1, 'bright', 3.)
     tracks = tbpm.get_all()
     assert len(tracks) > 0
--- database/track_timbre.py
+++ database/track_timbre.py
@@ -2,14 +2,14 @@ from database.database import Database
 from database.storinator import Storinator
 
 
-class TrackBPM(Storinator):
+class TrackTimbre(Storinator):
     def __init__(self, host="localhost", port=27017):
-        self._dbname = 'track_bpm'
+        self._dbname = 'track_timbre'
         self._db = Database(host, port)
 
-    def add(self, song_id, bpm, confidence):
+    def add(self, song_id, timbre, confidence):
         return self._db.insert(self._dbname, song_id, {
-            "bpm": bpm,
+            "timbre": timbre,
             "confidence": confidence
         })
 
