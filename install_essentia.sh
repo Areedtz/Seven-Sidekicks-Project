d944a96cdac08a6ca16e763d407b49b6f500d4af
Anders Sparrevohn <anders-sparrevohn@hotmail.com>
2019-03-13 17:06:10 +0100

    Added gaia to the travis config

--- install_essentia.sh
+++ install_essentia.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install build-essential libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
+sudo apt-get install build-essential libqt4-dev libyaml-dev swig python2-dev pkg-config
+sudo apt-get install libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
 sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
 
 cd ..
 
-git clone https://github.com/MTG/essentia
+git clone https://github.com/MTG/gaia/
+cd gaia
 
+./waf configure --with-python-bindings
+./waf
+./waf install
+
+cd ..
+
+git clone https://github.com/MTG/essentia
 cd essentia
-./waf configure --build-static --with-examples --with-python
+
+./waf configure --build-static --with-examples --with-python --with-gaia
 ./waf
 ./waf install
 
