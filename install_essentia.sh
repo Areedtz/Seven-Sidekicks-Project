7b6efc7f13e9d51b632113cb22bee31e98653a63
Anders Sparrevohn <anders-sparrevohn@hotmail.com>
2019-03-13 15:17:19 +0100

    Tried to fix the waf commands by adding sudo to it all

--- install_essentia.sh
+++ install_essentia.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 sudo apt-get update
 sudo apt-get install build-essential libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
 sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
@@ -8,8 +7,8 @@ sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
 git clone https://github.com/MTG/essentia
 
 cd essentia
-./waf configure --build-static --with-examples --with-python
-./waf
+sudo ./waf configure --build-static --with-examples --with-python
+sudo ./waf
 sudo ./waf install
 
 python -c 'import essentia'
