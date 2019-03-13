ebc6e093c72fae08b4b3bedbb30fde0cc6c41135
Anders Sparrevohn <anders-sparrevohn@hotmail.com>
2019-03-13 17:29:49 +0100

    Added a few comments and set a specific version for the swig package

--- install_essentia.sh
+++ install_essentia.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install build-essential libqt4-dev libyaml-dev swig pkg-config
-sudo apt-get install libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
-sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
-
 cd ..
+
+# Install gaia
+sudo apt-get update
+sudo apt-get install build-essential libqt4-dev libyaml-dev pkg-config
+sudo apt-get install swig3.0=3.0.12-1.2
 
 git clone https://github.com/MTG/gaia/
 cd gaia
@@ -15,6 +15,10 @@ python2 ./waf
 python2 ./waf install
 
 cd ..
+
+# Install essentia
+sudo apt-get install libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
+sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
 
 git clone https://github.com/MTG/essentia
 cd essentia
