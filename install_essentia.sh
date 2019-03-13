ddbcb5ce35b06093c98590ed6b526828e5792d53
Anders Sparrevohn <anders-sparrevohn@hotmail.com>
2019-03-13 14:23:42 +0100

    Changed the configure of waf with python3

--- install_essentia.sh
+++ install_essentia.sh
@@ -8,8 +8,8 @@ sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
 git clone https://github.com/MTG/essentia
 
 cd essentia
-python3 ./waf configure --with-python
-python3 ./waf
-sudo python3 ./waf install
+./waf configure --python=/home/travis/virtualenv/python3.6.7/bin/ --with-python
+./waf
+sudo ./waf install
 
 python -c 'import essentia'
