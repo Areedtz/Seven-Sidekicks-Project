141c113c7834ed141d028ba02ca9b59b10121e02
Anders Sparrevohn <anders-sparrevohn@hotmail.com>
2019-03-18 15:11:20 +0100

    Added caching for essentia and dependencies

--- install_essentia.sh
+++ install_essentia.sh
@@ -1,42 +1,59 @@
 #!/usr/bin/env bash
 
-cd ..
-
-# Install swig
-git clone https://github.com/swig/swig.git
-cd swig
-
-./autogen.sh
-./configure
-make
-sudo make install
-
-cd ..
-
-# Install gaia
 sudo apt-get update
-sudo apt-get install build-essential libqt4-dev libyaml-dev pkg-config
+sudo apt-get install -y build-essential libqt4-dev libyaml-dev pkg-config
 
-git clone https://github.com/MTG/gaia/
-cd gaia
+sudo apt-get install -y libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev
+sudo apt-get install -y python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
+sudo apt-get install -y python3-dev python3-numpy-dev python3-numpy python3-yaml
 
-python2 ./waf configure --with-python-bindings
-python2 ./waf
-sudo python2 ./waf install
+# Install Gaia2 if not already installed
+if [ ! -d "/usr/local/include/gaia2" ]; then
+    echo "Gaia not installed. Checking for swig..."
 
-cd ..
+    # Install Swig if not already installed
+    if [ ! -d "/usr/local/share/swig" ]; then
+        echo "Swig not installed. Installing swig..."
+        cd $HOME
 
-# Install essentia
-sudo apt-get install libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev libchromaprint-dev python-six
-sudo apt-get install python3-dev python3-numpy-dev python3-numpy python3-yaml
+        git clone https://github.com/swig/swig.git
+        cd swig
 
-git clone https://github.com/MTG/essentia
-cd essentia
+        ./autogen.sh
+        ./configure
+        make
+        sudo make install
+    fi
+    
+    echo "Swig installed. Installing gaia..."
+    cd $HOME
 
-./waf configure --build-static --with-examples --with-python --with-gaia
-./waf
-./waf install
+    git clone https://github.com/MTG/gaia/
+    cd gaia
 
-python -c 'import essentia'
+    python2 ./waf configure --with-python-bindings
+    python2 ./waf
+    sudo python2 ./waf install
 
-cd ../Seven-Sidekicks-Project
+    echo "Gaia installed."
+fi
+
+# Install Essentia if not already installed
+if [ ! -d "/usr/local/include/essentia" ]; then
+    echo "Essentia not installed. Installing Essentia..."
+    cd $HOME
+
+    git clone https://github.com/MTG/essentia
+    cd essentia
+
+    ./waf configure --build-static --with-examples --with-python --with-gaia
+    ./waf
+    ./waf install
+
+    python -c 'import essentia'
+    echo "Essentia installed."
+fi
+
+echo "All dependencies successfully installed. Going to tests..."
+
+cd $HOME/Seven-Sidekicks-Project
