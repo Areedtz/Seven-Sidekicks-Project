e27b3ad16bad5e04897b108cd8ee2ba3edb9bfd1
hoje <hoje@itu.dk>
2019-03-18 13:47:10 +0100

    Added a couple of tests to analyze_video and analyze_frame in facial_recognition

--- video_emotion/facial_recognition/t/test_facial_recognition.py
+++ video_emotion/facial_recognition/t/test_facial_recognition.py
@@ -1,28 +1,35 @@
 import os
 
-from video_emotion.facial_recognition.facial_recognition import analyze_video
+from video_emotion.facial_recognition.facial_recognition import analyze_video, analyze_frame
 
 def test_facial_recognition_of_at_least_one_face():
     dirname = os.path.dirname(__file__)
     test_filename = os.path.join(
         dirname, "test_facial_recognition/Fun_at_a_Fair.mp4")
 
-    output_files = analyze_video(test_filename, (3000, 5000))
+    output_frames = analyze_video(test_filename, (3000, 5000))
 
-    assert len(output_files) != 0
-'''
-def test_facial_recognition_of_two_faces_in_one_frame():
+    assert len(output_frames) != 0
+
+    frame_key = "80"
+    faces = output_frames.get(frame_key)
+
+    assert faces.__sizeof__ = 2
+
+def test_facial_recognition_of_two_faces_in_single_frame():
     dirname = os.path.dirname(__file__)
     test_filename = os.path.join(
         dirname, "test_facial_recognition/Fun_at_a_Fair.mp4")
 
-    output_files = facial_recognition(test_filename, (3000, 5000))
+    cap = cv2.VideoCapture(test_filename)
+    frame = cap.read()
+    timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+    if timestamp < 3080:
+        continue
+    elif timestamp > 3081:
+        break
 
-    output_file_name1 = os.path.join(
-        dirname, "output_files/80_0.png")
+    output_tuple = analyze_frame(frame)
 
-    output_file_name2 = os.path.join(
-        dirname, "output_files/80_1.png")
-
-    assert (os.path.exists(output_file_name1) and os.path.exists(output_file_name2))
-'''
+    assert len(output_tuple) != 0
+    assert len(output_tuple) = 2
