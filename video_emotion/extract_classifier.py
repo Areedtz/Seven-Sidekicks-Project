fb5792eaf807e93300f2104a1ce8a475d90609e8
Emil Ullerup Henriksen <emilbil40@gmail.com>
2019-03-18 13:35:15 +0100

    Surprise

--- video_emotion/extract_classifier.py
+++ video_emotion/extract_classifier.py
@@ -3,7 +3,7 @@ from emotionTagger.face_emotion_extraction import classify_face
 
 def classify_video(video_path,time_range=None):
     faces = analyze_video(video_path,time_range)
-    angry_sum = disgust_sum = fear_sum = happy_sum = sad_sum = suprise_sum = neutral_sum = 0.0
+    angry_sum = disgust_sum = fear_sum = happy_sum = sad_sum = surprise_sum = neutral_sum = 0.0
     for key,value in faces.items():
         for face in value:
             emotions = classify_face(face)
@@ -12,7 +12,7 @@ def classify_video(video_path,time_range=None):
             fear_sum += emotions[2]
             happy_sum += emotions[3]
             sad_sum += emotions[4]
-            suprise_sum += emotions[5]
+            surprise_sum += emotions[5]
             neutral_sum += emotions[6]
     numberOfFaces = len(faces)
     return {
@@ -21,7 +21,7 @@ def classify_video(video_path,time_range=None):
         "fear":fear_sum/numberOfFaces,
         "happy":happy_sum/numberOfFaces,
         "sad":sad_sum/numberOfFaces,
-        "suprise":suprise_sum/numberOfFaces,
+        "surprise":surprise_sum/numberOfFaces,
         "neutral":neutral_sum/numberOfFaces
     }
 
