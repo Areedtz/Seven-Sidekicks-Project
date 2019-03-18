ec977ddc5434ae511654f92f1563facbbbc29cdf
Lukas Holst <lukh@itu.dk>
2019-03-18 13:05:34 +0100

    Added test_get_labels_valid_input() and test_preprocess_input()

--- video_emotion/emotionTagger/t/test_face_emotion_extraction.py
+++ video_emotion/emotionTagger/t/test_face_emotion_extraction.py
@@ -1,7 +1,11 @@
 from video_emotion.emotionTagger.face_emotion_extraction import classify_face
+from video_emotion.emotionTagger.face_emotion_extraction import get_labels
+from video_emotion.emotionTagger.face_emotion_extraction import preprocess_input
+import numpy as np
 import cv2
 import os
-def testAngryFace():
+
+def test_angry_face():
     dirname = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(
         dirname,
@@ -11,7 +15,7 @@ def testAngryFace():
     assert len(faceData) == 7
     assert faceData[0] > 0.5
 
-def testHappy1():
+def test_happy_1():
     dirname = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(
         dirname,
@@ -21,7 +25,7 @@ def testHappy1():
     assert len(faceData) == 7
     assert faceData[3] > 0.5
 
-def testHappy2():
+def test_happy_2():
     dirname = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(
         dirname,
@@ -31,7 +35,7 @@ def testHappy2():
     assert len(faceData) == 7
     assert faceData[3] > 0.5
 
-def testHappy3():
+def test_happy_3():
     dirname = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(
         dirname,
@@ -41,7 +45,7 @@ def testHappy3():
     assert len(faceData) == 7
     assert faceData[3] > 0.3
     
-def testNeutral1():
+def test_neutral_1():
     dirname = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(
         dirname,
@@ -50,5 +54,24 @@ def testNeutral1():
     faceData = classify_face(im)
     assert len(faceData) == 7
     assert faceData[6] > 0.5
+
+def test_get_labels_valid_input():
+    dataset_expected = {0:'angry', 1:'disgust', 2:'fear',
+                        3:'happy', 4:'sad', 5:'surprise',
+                        6:'neutral'}
+
+    result = get_labels('fer2013')
+
+    assert dataset_expected == result
+
+def test_preprocess_input():
+    expected = 0.9607843137254901
+
+    tf = np.float32(250) # testing float
+    result = preprocess_input(tf, True)
+
+    assert (expected == result)
+
+
     
     
