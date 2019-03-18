e5a9209a277b73f94054b15f44fff0c0377599af
hoje <hoje@itu.dk>
2019-03-18 14:23:46 +0100

    Removed currently not correctly implemented test of analyze_frame (indirectly tested through analyze_video)

--- video_emotion/facial_recognition/t/test_facial_recognition.py
+++ video_emotion/facial_recognition/t/test_facial_recognition.py
@@ -16,7 +16,7 @@ def test_facial_recognition_of_at_least_one_face():
     faces = output_frames.get(frame_key)
 
     assert len(faces) == 2
-
+'''
 def test_facial_recognition_of_two_faces_in_single_frame():
     dirname = os.path.dirname(__file__)
     test_filename = os.path.join(
@@ -24,13 +24,15 @@ def test_facial_recognition_of_two_faces_in_single_frame():
 
     cap = cv2.VideoCapture(test_filename)
     frame = cap.read()
+    output_tuple = {}
     while(cap.isOpened()):
         timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
-        if timestamp < 3080:
+        if timestamp < 3079:
             continue
-        elif timestamp > 3081:
+        elif timestamp > 3082:
             break
         output_tuple = analyze_frame(frame)
 
     assert len(output_tuple) != 0
     assert len(output_tuple) == 2
+'''
