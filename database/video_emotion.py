63b9a8884fc14740fc4ccb5552fe480a3a96c6b5
Lukas Holst <lukh@itu.dk>
2019-03-18 15:54:34 +0100

    Created class with methods for sending data to database

---
+++ database/video_emotion.py
@@ -0,0 +1,17 @@
+from database.video_emotion_database import VEDatabase
+from database.storinator import Storinator
+
+
+class VideoEmotion(Storinator):
+    def __init__(self, host="localhost", port=27017, username=None, password=None):
+        self._dbname = 'video_emotion'
+        self._db = VEDatabase(host, port, username, password)
+
+    def add(self, song_id, video_id, emotions):
+        return self._db.insert(self._dbname, song_id, video_id, emotions)
+
+    def get(self, song_id, video_id):
+        return self._db.find(self._dbname, song_id, video_id)
+
+    def get_all(self):
+        return self._db.find_all(self._dbname)
