1f6cdc19facb7d6b71c47e2f769b6d7b3108898c
Casper Færch <cafa@html24.net>
2019-03-25 16:25:29 +0100

    Issues with Json decoding

--- rest_api/application.py
+++ rest_api/application.py
@@ -1,20 +1,63 @@
+import sys
+import os
+import json
+
+# Start of importing the utilities module
+#sys.path.insert(0, os.path.abspath("../"))
+#import bpm.bpm_extractor as bpm_extractor
+
+# End of importing the utilities module
+
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask import request
+from flask_restplus import Resource, Api, reqparse, fields
+from pathlib import PurePath
+from json import JSONDecoder
 
 app = Flask(__name__)
 api = Api(app)
+
+# We need to find the right way to set a custom json decoder
+# the proper way. That shit below is improper
+app.json_decoder = json.loads
 
 hostURL = "0.0.0.0"
 hostPort = 1337
 apiRoute = '/hello'
 
-def getAPIInfo():
-    return (hostURL, hostPort)
-
 @api.route(apiRoute)
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'} 
+        return {'hello': 'world'}
 
+# Model is nested inside the song_fields model
+id_model = api.model('IdModel', {
+    'Release': fields.Integer(description='The release ID of the song', required=True),
+    'Side': fields.Integer(description='The side of the media', required=True),
+    'Track': fields.Integer(description='The tracknumber of the media', required=True),
+})
+
+song_fields = api.model('SongModel', {
+    'ID': fields.Nested(id_model, description='ID model of the song to analyze', required=False),
+    'SourcePath': fields.String(description='The path of the song to analyze', required=True),
+    'User': fields.String(description='The requesting user', required=True),
+})
+
+@api.route('/analyze_song')
+class AnalyzeSong(Resource):
+
+    @api.doc(body=song_fields)
+    def post(self):
+        a = request.get_json()
+        return 'a'
+        
+@api.route('/shutdown')
+class Shutdown(Resource):
+    def get(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+    
 if __name__ == '__main__':
     app.run(host=hostURL, port=hostPort, debug=True)
--- rest_api/t/test_application.py
+++ rest_api/t/test_application.py
@@ -6,12 +6,21 @@ import time
 def start_rest():
     rest.app.run(host=rest.hostURL, port=rest.hostPort, debug=False)
 
-t = threading.Thread(target=start_rest)
-t.start()
 
-time.sleep(2)
+def test_rest_server():
+    t = threading.Thread(target=start_rest)
 
-r = requests.get("http://" + rest.hostURL + ":" + str(rest.hostPort) + rest.apiRoute)
-        
-assert(r.json is not None)
+    t.start()
+
+    number_of_tries = 0
+    while number_of_tries < 10:
+        try:
+            r = requests.get("http://" + rest.hostURL + ":" + str(rest.hostPort) + rest.apiRoute)
+            requests.get("http://" + rest.hostURL + ":" + str(rest.hostPort) + "/shutdown")
+            break
+        except requests.exceptions.ConnectionError:
+            number_of_tries += 1
+            time.sleep(0.5)
+
+    assert(r.json is not None)
 
