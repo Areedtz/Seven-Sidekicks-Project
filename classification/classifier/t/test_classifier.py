f2dfc424507be9b98818224be6bab01ee8f9124d
hoje <hoje@itu.dk>
2019-03-25 13:58:30 +0100

    final push of adding moods to profile

--- classification/classifier/t/test_classifier.py
+++ classification/classifier/t/test_classifier.py
@@ -17,4 +17,4 @@ def test_profile_song_data():
     assert song_data[2] == ("not_party", 0.643296301365)
     assert song_data[3] == ("aggressive", 0.993713259697)
     assert song_data[4] == ("not_happy", 0.926596462727)
-    assert song_data[5] == ("sad", 0.9)
+    assert song_data[5] == ("not_sad", 0.977098941803)
