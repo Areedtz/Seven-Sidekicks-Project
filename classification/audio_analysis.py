dc642d6d2a42c7cbcddc6e1024eedb6ee7fe84f6
hoje <hoje@itu.dk>
2019-03-27 10:20:10 +0100

    Changed audio_analysis to be run seperate from a function - made test an actual test

--- classification/audio_analysis.py
+++ classification/audio_analysis.py
@@ -2,50 +2,63 @@ import os
 import sys
 import re
 import csv
+import subprocess
 
 # Start of importing the utilities module
 sys.path.insert(0, os.path.abspath("../utilities/"))
-import get_song_id as s_id
+import utilities.get_song_id as s_id
 # End of importing the utilities module
 
 from extractor.low_level_data_extractor import make_low_level_data_file
 from classifier.profile_data_extractor import get_classifier_data
 from similarity.split_song import split_song
 
-from pprint import pprint
-from tabulate import tabulate
-
 from multiprocessing import Pool
+
 
 def process_data_and_extract_profiles(segment_id, song_file, song_output_file):
     make_low_level_data_file(song_file, song_output_file)
 
     timbre, mood_relaxed, mood_party, mood_aggressive, mood_happy, mood_sad = get_classifier_data(song_output_file)
 
+    #removing the segments classifier data file
+    subprocess.run("rm {}".format(song_output_file), shell=True)
+
     return segment_id, timbre, mood_relaxed, mood_party, mood_aggressive, mood_happy, mood_sad
 
+ 
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit()
 
-def segment_song_and_extract_profile_data(song_file):
-    # Go through all .wav files in the given directory
+    arg = sys.argv[1]
+
     dirname = os.path.abspath(os.path.dirname(__file__))
     output_folder_path = os.path.join(dirname, "output/")
     argument_triples = []
 
-    song_id = s_id.get_song_id(song_file)
+    song_id = s_id.get_song_id(arg)
     
-    split_song_list = split_song(song_file)
+    split_song_list = split_song(arg)
 
-    for i in range(split_song_list.__len__):
+    for i in range(split_song_list.__len__-1):
         song_output_file = "{}{}_{}_output.json".format(
                 output_folder_path, i, song_id)
-                
+
         argument_triples.append((
             song_id,
             split_song_list[i],
             song_output_file))
 
-        #make_low_level_data_file(split_song_list[i], song_output_file)
+    csv_output_file = "{}{}_segmented_output.csv".format(
+            output_folder_path, song_id)
 
+    # CSV file header
+    with open(csv_output_file, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([song_id, 'Segment ID', 'Timbre', 'Mood Relaxed', 'Mood Party', 'Mood Aggressive', 'Mood Happy', 'Mood Sad'])
+
+    csv_file.close()
 
     # Multithreaded runthrough of all files
     pool = Pool(8)
@@ -54,12 +67,9 @@ def segment_song_and_extract_profile_data(song_file):
 
     csv_data = [res]
 
-    csv_output_file = "{}{}_output.csv".format(
-            output_folder_path, song_id)
-
     with open(csv_output_file, 'a') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerows(csv_data)
 
     csv_file.close()
- 
+
--- classification/t/audio_analysis_test.py
+++ classification/t/audio_analysis_test.py
@@ -1,14 +1,32 @@
 import os
 import sys
 
-from classification.audio_analysis import segment_song_and_extract_profile_data
+from classification.audio_analysis import process_data_and_extract_profiles
+import utilities.get_song_id as s_id
+from similarity.split_song import split_song
+
 
 def test_profile_song_data():
     dirname = os.path.abspath(os.path.dirname(__file__))
+    output_folder_path = os.path.join(dirname, "output/")
     filename = os.path.join(
         dirname,
         "test_audio_analysis/8376-1-1_Demolition_Man_proud_music_preview.wav")
+    result = []
 
-    segment_song_and_extract_profile_data(filename)
+    song_id = s_id.get_song_id(filename)
+    
+    split_song_list = split_song(filename)
 
-    assert song_data != None
+    for i in range(split_song_list.__len__-1):
+        song_output_file = "{}{}_{}_output.json".format(
+                output_folder_path, i, song_id)
+        
+        result.append(
+            process_data_and_extract_profiles(
+                song_id, 
+                split_song_list[i], 
+                song_output_file))
+
+
+    assert result.__len__ != 0
