50b974042f97eb032e0c7940a7200152e5d6f99c
Emil Ullerup Henriksen <emilbil40@gmail.com>
2019-03-27 14:01:36 +0100

    finally fixed headers

--- utilities/filehandler/handle_path.py
+++ utilities/filehandler/handle_path.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_absolute_path(path):
-    dirname = os.path.abspath(os.path.dirname(__file__)) + "/.."
+    dirname = os.path.abspath(os.path.dirname(__file__)) + "/../.."
     return os.path.join(
         dirname,
         path)
