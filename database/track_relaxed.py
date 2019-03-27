fe3186aa5e942a5b6545c00831f91dc128f572d6
Lukas Holst <lukh@itu.dk>
2019-03-27 15:10:01 +0100

    Changed lines to uphold 78 line pep8 reqs

--- database/track_relaxed.py
+++ database/track_relaxed.py
@@ -3,7 +3,8 @@ from database.storinator import Storinator
 
 
 class TrackRelaxed(Storinator):
-    def __init__(self, host="localhost", port=27017, username=None, password=None):
+    def __init__(self, host="localhost", port=27017, 
+                 username=None, password=None):
         self._dbname = 'track_relaxed'
         self._db = Database(host, port, username, password)
 
