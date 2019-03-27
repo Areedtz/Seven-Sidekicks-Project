e8e748ecee7ac6cd213474c3da6d008a88f3a070
Emil Ullerup Henriksen <emilbil40@gmail.com>
2019-03-27 13:11:18 +0100

    headers

--- bpm/t/test_bpm_extractor.py
+++ bpm/t/test_bpm_extractor.py
@@ -4,10 +4,9 @@ import sys
 # Start of importing the utilities module
 sys.path.insert(0, os.path.abspath("utilities/"))
 
-#import get_song_id as s_id
 # End of importing the utilities module
 
-from bpm.bpm_extractor import get_song_bpm, s_id
+from bpm.bpm_extractor import get_song_bpm
 from filehandler.handle_audio import get_MonoLoaded_Song
 
 
