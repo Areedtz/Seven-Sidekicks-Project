69f6b2e517c74298ca72d8874665e0b52e4f335e
Emil Ullerup Henriksen <emilbil40@gmail.com>
2019-04-01 10:56:26 +0200

    Fixed test

--- video_emotion/t/test_extract_test_data.py
+++ video_emotion/t/test_extract_test_data.py
@@ -5,7 +5,8 @@ def test_generate_log_data_formats_as_expected():
     data = generate_log_data(500, [{"happy": 10, "angry": 10, "fear": 10,
                                     "disgust": 5, "sad": 5, "surprise": 5,
                                     "neutral": 5}])
-    assert data == ("From,To,emotion_disgust,"
+    assert data == ("From,To,"
+                    + "emotion_angry,emotion_disgust,"
                     + "emotion_fear,emotion_happy,emotion_sad,"
                     + "emotion_surprise,emotion_neutral\n"
                     + "0,500,10,5,10,10,5,5,5\n")
