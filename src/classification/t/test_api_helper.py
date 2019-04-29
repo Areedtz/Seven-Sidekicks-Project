ad18770c603f9b7a88895b9bae1d3a7e1073cbba
Lukas Hundt Petersen <lukashundtpetersen@gmail.com>
2019-04-29 11:50:25 +0200

    Try using absolute path for test

--- src/classification/t/test_api_helper.py
+++ src/classification/t/test_api_helper.py
@@ -2,11 +2,12 @@ import sys
 
 from classification.api_helper import process_data_and_extract_profiles
 from database.track_emotion import TrackEmotion
+import utilities.filehandler.handle_path as path_handler
 
 
 def test_api_helper():
     id = "1337"
-    file_path = "classification/t/test_segmented_audio_analysis/8376-1-1_Demolition_Man_proud_music_preview.wav"
+    file_path = path_handler.get_absolute_path("classification/t/test_segmented_audio_analysis/8376-1-1_Demolition_Man_proud_music_preview.wav")
 
     process_data_and_extract_profiles(id, file_path)
 
