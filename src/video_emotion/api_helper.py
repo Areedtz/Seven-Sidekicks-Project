7fe5436991d5f52482ce0e46163deeb177aa877b
Emil Ullerup Henriksen <emilbil40@gmail.com>
2019-05-06 16:15:26 +0200

    Added documentation

--- src/video_emotion/api_helper.py
+++ src/video_emotion/api_helper.py
@@ -1,11 +1,30 @@
-import json
-import os
+from typing import Dict
 
 from video_emotion.extract_classifier import classify_video
 from database.video_emotion_no_song import VideoEmotionNS
 from database.video_emotion import VideoEmotion
 
-def process_data_and_extract_emotions(video_id, video_path, time_range):
+
+def process_data_and_extract_emotions(video_id: int, video_path: str, time_range:Dict[str,int]) -> bool:
+    """Clasifies the emotions in a video
+
+    Parameters
+    ----------
+    video_id
+        The id of the video to analyze
+
+    video_path
+        The path of the video that is to be analyzed
+
+    time_range
+        The time range as a dictionary to analyze
+
+    Returns
+    -------
+    bool
+        The boolean describing whether it succeeded
+    """
+
     data = classify_video(video_path, (time_range['From'], time_range['To']))
     vet = VideoEmotionNS()
     vet.add(video_id, time_range, data)
@@ -14,6 +33,28 @@ def process_data_and_extract_emotions(video_id, video_path, time_range):
 
 def process_data_and_extract_emotions_with_song(video_id, video_path, 
                                                 time_range, song_id):
+    """Clasifies the emotions in a video and places it in the database with a songid
+
+        Parameters
+        ----------
+        video_id
+            The id of the video to analyze
+
+        video_path
+            The path of the video that is to be analyzed
+
+        time_range
+            The time range as a dictionary to analyze
+
+        song_id
+            The song to associate the video with
+
+        Returns
+        -------
+        string
+            Returns a csv of the emotions in the emotion set
+        """
+
     data = classify_video(video_path, (time_range['From'], time_range['To']))
     vet = VideoEmotion()
     vet.add(video_id, song_id, time_range, data)
--- src/video_emotion/emotionTagger/face_emotion_extraction.py
+++ src/video_emotion/emotionTagger/face_emotion_extraction.py
@@ -2,22 +2,61 @@ import cv2
 import numpy
 from keras.models import load_model
 from keras import backend as K
+from typing import Dict
 
 from utilities.filehandler.handle_path import get_absolute_path
 
 
-def get_labels():
+def get_labels() -> Dict[int, str]:
+
+    """Get the labels for each argument in the emotion array
+
+        Returns
+        -------
+        string
+            Returns the labels for each emotion as a dictionary
+        """
+
     return {0: 'angry', 1: 'disgust', 2: 'fear',
             3: 'happy', 4: 'sad', 5: 'surprise',
             6: 'neutral'}
 
 
-def preprocess_input(x):
+def preprocess_input(x: float) -> float:
+    """Preprocesses input
+
+    Parameters
+    ----------
+    float
+        The float to be proccessed
+
+
+            Returns
+            -------
+            float
+                Returns the input, but preprocessed
+            """
+
     x = x.astype('float32')
     return ((x / 255.0) - 0.5) * 2.0
 
 
 def classify_faces(faces):
+
+    """Classifies the faces inputed
+
+    Parameters
+    ----------
+    faces
+        The faces to be processed
+
+        Returns
+        -------
+        array
+              Returns a 2d array containing the face emotion data, for each face
+               is an array index with a subarray containing the emotions indexed like in getlabels
+        """
+
     # parameters for loading data and images
     emotion_model_path = get_absolute_path("video_emotion/"
                                            + "emotionTagger/"
