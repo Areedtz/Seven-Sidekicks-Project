4f5ce4958e1f1c8e578de5ded21faff63e58767c
mikkelkai <mikkelkai@gmail.com>
2019-05-06 13:52:19 +0200

    Remove unecessary check

--- src/database/database.py
+++ src/database/database.py
@@ -29,8 +29,8 @@ class Database:
 
         self._client = MongoClient(
             cfg['mongo_host'], cfg['mongo_port'],
-            username=cfg['mongo_user'] if 'mongo_user' in cfg else None,
-            password=cfg['mongo_pass'] if 'mongo_pass' in cfg else None)
+            username=cfg['mongo_user'],
+            password=cfg['mongo_pass'])
         self._db = self._client[cfg['mongo_db']]
 
     def insert(self, col, song_id, doc):
