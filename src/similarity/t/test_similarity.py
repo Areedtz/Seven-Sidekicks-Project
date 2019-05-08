afe78511f05c331b88af78229588e5c56fe69e6a
mikkelkai <mikkelkai@gmail.com>
2019-05-08 08:16:05 +0200

    A few tests and some documentation

--- src/similarity/t/test_similarity.py
+++ src/similarity/t/test_similarity.py
@@ -1,48 +1,61 @@
 import numpy as np
 
-from similarity.similarity import process_segment, load_songs, process_db_segment, create_feature, dist, create_bucket, find_best_matches, query_similar, find_matches, analyze_songs
+import falconn
+from similarity.similarity import _load_songs, _dist, _create_bucket, query_similar, _find_matches, analyze_songs
+from utilities.config_loader import load_config
 
-
-def test_process_segment():
-    assert True
+cfg = load_config()
+MATCHES = cfg['similarity_matches']
 
 
 def test_load_songs():
-    assert True
-
-
-def test_process_db_segment():
-    assert True
-
-
-def test_create_feature():
-    assert True
+    segments = _load_songs(
+        [('8376-1-1', 'similarity/t/test_split_song/8376-1-1_Demolition_Man_proud_music_preview.wav')])
+    assert len(segments) == 18
+    assert segments[0][1] == '8376-1-1'
+    assert segments[0][2] == 0
 
 
 def test_create_bucket():
-    assert True
+    segments = _load_songs(
+        [('8376-1-1', 'similarity/t/test_split_song/8376-1-1_Demolition_Man_proud_music_preview.wav')])
+
+    data = np.array(list(map(lambda x: x[3], segments)))
+
+    segs, table = _create_bucket(data)
+    assert np.array_equal(data, segs)
+    assert isinstance(table, falconn.LSHIndex)
 
 
 def test_dist():
-    start = np.array([0, 0])
-    end = np.array([0, 1])
-    assert dist(start, end) == 1
+    start = ('', '', 0, np.array([0, 0]))
+    end = ('', '', 1, np.array([0, 1]))
+    assert _dist(start, end) == 1
 
-    end = np.array([1, 0])
-    assert dist(start, end) == 1
+    end = ('', '', 2, np.array([1, 0]))
+    assert _dist(start, end) == 1
 
 
-def test_find_best_matches():
+def test_find_matches():
+    segments = _load_songs(
+        [('8376-1-1', 'similarity/t/test_split_song/8376-1-1_Demolition_Man_proud_music_preview.wav')])
+
+    data = np.array(list(map(lambda x: x[3], segments)))
+
+    bucket = _create_bucket(data)
+
+    query_object = bucket[1].construct_query_pool()
+    query_object.set_num_probes(25)
+
+    matches = list(map(_find_matches, list(
+        map(lambda seg: (seg, query_object), segments))))
+
+    assert len(matches) == MATCHES
+
+
+def test_analyze_songs():
     assert True
 
 
 def test_query_similar():
-    assert True
-
-
-def test_find_matches():
-    assert True
-
-
-def test_analyze_songs():
     assert True
